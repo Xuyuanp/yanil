8aeb17f4dea7473ba5ac7e8c0809ce64fe8332b0
Xuyuan Pang <xuyuanp@gmail.com>
2020-10-22 18:39:53 +0800

    Removed sample config

--- lua/yanil.lua
+++ lua/yanil.lua
@@ -1,7 +1,5 @@
-local git        = require("yanil/git")
-local decorators = require("yanil/decorators")
-local devicons   = require("yanil/devicons")
-local canvas     = require("yanil/canvas")
+local git      = require("yanil/git")
+local devicons = require("yanil/devicons")
 
 local config = {
     colors   = require("yanil/colors"),
@@ -17,47 +15,6 @@ function M.setup(opts)
     config.commands.setup()
     git.setup(opts.git)
     devicons.setup(opts.devicons)
-end
-
-function M.apply_authors_config()
-    local header = require("yanil/sections/header"):new()
-    local tree = require("yanil/sections/tree"):new()
-
-    tree:setup {
-        draw_opts = {
-            decorators = {
-                decorators.pretty_indent_with_git,
-                devicons.decorator(),
-                decorators.space,
-                decorators.default,
-                decorators.executable,
-                decorators.readonly,
-                decorators.link_to,
-            }
-        },
-        keymaps = {
-            ["]c"] = git.jump_next,
-            ["[c"] = git.jump_prev,
-        }
-    }
-
-    canvas.register_hooks {
-        on_enter = function() git.update(tree.cwd) end,
-    }
-
-    canvas.setup {
-        sections = {
-            header,
-            tree,
-        },
-        autocmds = {
-            {
-                event = "User",
-                pattern = "YanilGitStatusChanged",
-                cmd = function() tree:refresh() end,
-            },
-        }
-    }
 end
 
 return M
