906b34ce928f6cf5914db49b8978ba5b208d834a
Xuyuan Pang <xuyuan.pang@tusimple.ai>
2022-01-27 18:07:32 +0800

    Fixed binary detecting

--- lua/yanil/utils.lua
+++ lua/yanil/utils.lua
@@ -67,7 +67,7 @@ end
 function M.is_binary(path)
     validate { path = { path, "string" } }
 
-    local output = vim.fn.system("file --mime -L -n -N -p -b -d " .. path)
+    local output = vim.fn.system("file -binpLN " .. path)
     if vim.v.shell_error > 0 then
         api.nvim_err_writeln(string.format("check file %s mime encoding failed: %s", path, output))
         return
